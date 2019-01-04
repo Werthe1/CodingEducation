ee0cdf52bc1e6c1c9757ef75de60b9f25a0219bd
kimhyeri <hyer1k@naver.com>
2019-01-04 18:18:38 +0900

    change indexpath row to section

--- CodingEducation/Controller/SelectViewController.swift
+++ CodingEducation/Controller/SelectViewController.swift
@@ -14,8 +14,23 @@ class SelectViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
     }
     
 
+}
+
+extension SelectViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
 }
