8de21698945c5db6dd618c4f69424827282d91e2
kimhyeri <hyer1k@naver.com>
2019-01-06 12:55:33 +0900

    add temp data

--- CodingEducation/Controller/SelectViewController.swift
+++ CodingEducation/Controller/SelectViewController.swift
@@ -12,6 +12,8 @@ class SelectViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var ranking = ["김혜리":3,"이지윤":2,"김준용":2,"김만기":1,"박소희":1,"김보미":1,"고은진":1,"이혜민":1,"김혜진":1,"박혜리":1,"정민정":1,"이유나":1,"이태낭":1,"정나현":1,"김혜민":1,"김정훈":1,"윤진한":1,"최예준":1,"윤택한":1,"박봄":1,"박하은":1,"최희준":1]
+    var select = [6,4,3,2,2,2,2,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0]
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
@@ -27,11 +29,14 @@ extension SelectViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RankingTableViewCell
+        cell.nameLabel.text = ranking.map({$0.key})[indexPath.row]
+        cell.levelLabel.text = "level: " + String(ranking.map({$0.value}).sorted().reversed()[indexPath.row])
+        cell.selectLabel.text = "채택받은 답 \(select.sorted().reversed()[indexPath.row])"
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return ranking.count
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
